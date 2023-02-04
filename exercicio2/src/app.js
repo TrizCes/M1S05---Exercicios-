bd0597c3ded73f12e04579333568ebfdcec8e709
Beatriz Ceschini <biaceschini@gmail.com>
2023-02-03 21:50:17 -0300

    review ex2

--- exercicio2/src/app.js
+++ exercicio2/src/app.js
@@ -1,5 +1,5 @@
 import Fatura from './Fatura.js';
-
+//lancar faturas:
 const melao = new Fatura(123, 'Melão', 2, 3);
 const valor = melao.obterValorTotal();
 console.log(valor);
@@ -8,10 +8,11 @@ const laranja = new Fatura(321, 'Laranja', 8, 2.99);
 const segundoValor = laranja.obterValorTotal();
 console.log(segundoValor);
 
-const pera = new Fatura(231, 'Pera', 0, 5);
+const pera = new Fatura(231, 'Pera', 1, 5);
 const terceiroValor = pera.obterValorTotal();
 console.log(terceiroValor);
 
+//getter:
 console.log('Extra : metodo getter');
 console.log(melao.valorTotal);
 console.log(laranja.valorTotal);
